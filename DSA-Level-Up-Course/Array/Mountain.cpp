128c83e59ec35d63d2126528edaadbefa2e54bde
Martin <martinayush04@gmail.com>
2022-06-24 20:32:09 +0530

    Mountain Code

--- DSA-Level-Up-Course/Array/Mountain.cpp
+++ DSA-Level-Up-Course/Array/Mountain.cpp
@@ -3,40 +3,35 @@ using namespace std;
 
 int main()
 {
-    // ans = 9;
-    //                     ------------------------
-    vector<int> a = {6, 5, 1, 2, 3, 4, 5, 4, 3, 2, 0, 1, 2, 3, -2, 4};
+    vector<int> a = {5, 6, 1, 2, 3, 4, 5, 4, 3, 2, 0, 1, 2, 3, -2, 4};
     int n = a.size();
-    // find peak and move forward and backward
-
     int ans = INT_MIN;
-    for (int i = 1; i < n - 1; i++)
+    for (int i = 1; i < n - 1;)
     {
-        // find peak
         if (a[i] > a[i - 1] && a[i] > a[i + 1])
         {
-            int count = 0;
-            // count backward
-            cout << "backward ";
-            for (int j = i - 1; j >= 1; j--)
+            int count = 1;
+
+            // backwards
+            int j = i;
+            while (j >= 1 && a[j] > a[j - 1])
             {
-                if (a[j] > a[j - 1])
-                {
-                    count++;
-                    cout << a[j] << " ";
-                }
+                count++;
+                j--;
             }
-            // count forward
-            cout << "fordward ";
-            for (int j = i + 1; j < n - 1; j++)
+
+            // forward
+            while (i < n - 1 && a[i] > a[i + 1])
             {
-                if (a[j] > a[j + 1])
-                {
-                    count++;
-                    cout << a[j] << " ";
-                }
+                count++;
+                i++;
             }
+
             ans = max(ans, count);
+        }
+        else
+        {
+            i++;
         }
     }
     cout << ans;
